8d192ad5eb945e0de4beb15158068f0881d170aa
Vladi <506727700@qq.com>
2025-08-29 14:08:55 +0800

    commit 4 finished hw01 and download hw02, lab02 and hog

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i * i + j * j + k * k - max(i, j, k) * max(i, j, k)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    for i in range(2, n):
+        if n % i == 0:
+            return n // i
+    return 1
+
 
 
 def hailstone(n):
@@ -88,4 +93,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    seq = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        seq.append(n)
+    for i in seq:
+        print(i)
+    return len(seq)
